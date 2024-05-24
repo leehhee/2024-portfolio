09424161d1e7459feaa8125122cef42fdcdc6cad
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-05-24 17:13:59 +0900

    feat

--- src/components/section/Visual.tsx
+++ src/components/section/Visual.tsx
@@ -1,13 +1,48 @@
 'use client';
-import { easeInOut, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
+
+import { useGSAP } from '@gsap/react';
+import { gsap } from 'gsap';
+
 import { skill } from '@/components/ui/icon';
 import { Section } from '@/components/ui/section';
 import { Ticker } from '@/components/ui/ticker';
 import { Badge } from '@/components/ui/button';
+import { useRef } from 'react';
+import SplitType from 'split-type';
 
 const tickerIcon = Object.entries(skill);
 
+gsap.registerPlugin(useGSAP);
+
 const Visual = () => {
+  const textContainerRef = useRef<HTMLParagraphElement>(null);
+
+  useGSAP(() => {
+    const text = new SplitType('.visual__text', { types: 'chars' });
+
+    const tl = gsap.timeline();
+
+    // tl.to(textContainerRef, {
+    //   duration: 3,
+    //   text:
+    // });
+    gsap.fromTo(
+      text,
+      {
+        y: 100,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.05,
+        duration: 10,
+        ease: 'power4.out',
+      }
+    );
+  }, []);
+
   return (
     <Section className='visual' id='visual'>
       <h2 className='sound-only'>비주얼 영역</h2>
@@ -22,7 +57,7 @@ const Visual = () => {
         </div>
         Available for Work
       </Badge>
-      <p className='visual__text'>
+      <p className='visual__text' ref={textContainerRef}>
         <span className='light'>Hello, </span>
         <br />
         I'm a Front-end
