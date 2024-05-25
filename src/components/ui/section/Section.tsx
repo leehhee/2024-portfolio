8a7d18746af856b7e69b01303c4af41ad1b75627
kimbangul <highcolor_12@naver.com>
2024-05-25 15:52:49 +0900

    feat: title 애니메이션 추가

--- src/components/ui/section/Section.tsx
+++ src/components/ui/section/Section.tsx
@@ -1,19 +1,38 @@
-import { motion, useScroll, useSpring } from 'framer-motion';
-import React, { RefObject, useRef } from 'react';
+import { motion } from 'framer-motion';
+import React, { useRef } from 'react';
 import { HTMLAttributes } from 'react';
-import { springOption } from '@/utils';
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+import SplitType from 'split-type';
+
+gsap.registerPlugin(useGSAP);
 
 const Section = React.forwardRef<HTMLDivElement, ISectionProps>(
   (props, ref) => {
-    const sectionRef = useRef(null);
-    const { scrollYProgress } = useScroll({
-      target: ref as RefObject<HTMLDivElement>,
-      offset: ['0 1'],
-    });
+    const titleRef = useRef<HTMLHeadingElement>(null);
 
-    const animateValue = {
-      scaleX: useSpring(scrollYProgress, springOption),
-    };
+    useGSAP(() => {
+      const section = document.querySelector(`.${props.className}`);
+      if (!titleRef?.current) return;
+      const text = new SplitType(titleRef.current, {
+        types: 'chars',
+      });
+      gsap.from(text.chars, {
+        y: 40,
+        opacity: 0,
+        scale: 0.85,
+        rotate: 15,
+        duration: 0.5,
+        stagger: 0.1,
+        yoyo: true,
+
+        scrollTrigger: {
+          trigger: section,
+          start: 'top center',
+          toggleActions: 'play none none reverse',
+        },
+      });
+    }, []);
 
     return (
       <section
@@ -29,7 +48,9 @@ const Section = React.forwardRef<HTMLDivElement, ISectionProps>(
         >
           {props.title && (
             <div className={`sc__title ${props.className}__title`}>
-              <h2 className='sc__title-text'>{props.title}</h2>
+              <h2 className='sc__title-text' ref={titleRef}>
+                {props.title}
+              </h2>
               <motion.span
                 initial={{ scaleX: 0, opacity: 0 }}
                 whileInView={{ scaleX: 1, opacity: 0.3 }}
@@ -52,3 +73,6 @@ export interface ISectionProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 export default Section;
+function useForwardRef<T>(ref: React.ForwardedRef<HTMLDivElement>) {
+  throw new Error('Function not implemented.');
+}
